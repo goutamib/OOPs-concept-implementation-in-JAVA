d34a44af48548e3c9ba217dde9fd285cd40d13c0
goutamib <goutami.bonam@gmail.com>
2022-05-02 15:26:13 +0530

    Add files via upload

---
+++ Squareroot.java
@@ -0,0 +1,14 @@
+package newpackage;
+
+public class Squareroot  {
+
+	
+	public static int getsqrt(double x) {
+		
+		int result=(int)Math.sqrt(x);
+		return result;
+	}
+	public static void main(String args[]) {
+		System.out.println(getsqrt(8));
+	}
+}
